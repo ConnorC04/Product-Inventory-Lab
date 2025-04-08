659f5812548a2c2c1d0f2afe8dd3ea5b07e32a27
ConnorC04 <connorjcasey@iCloud.com>
2025-04-08 15:25:15 -0400

    getters and setters

--- src/main/java/Models/Sneaker.java
+++ src/main/java/Models/Sneaker.java
@@ -1,4 +1,78 @@
 package Models;
 
 public class Sneaker {
+
+    private int id;
+    private String name;
+    private String brand;
+    private String sport;
+    private float size;
+    private int quantity;
+    private float price;
+
+    public Sneaker(int id, String name, String brand, String sport, float size, int quantity, float price){
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.sport = sport;
+        this.size = size;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public float getSize() {
+        return size;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setSize(float size) {
+        this.size = size;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
 }
